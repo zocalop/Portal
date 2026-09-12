7a1b6b8110b9103d974d40ae6bcdeb573503e7f1
zocalop <jcb2861@gmail.com>
2026-09-11 22:49:19 -0300

    Add input user name form upon Portal message click

--- src/Components/Portal.jsx
+++ src/Components/Portal.jsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react';
 
 function Portal() {
   const [scroll, setScroll] = useState(0);
+  const [showForm, setShowForm] = useState(false);
+  const [showMessage, setShowMessage] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -17,17 +19,41 @@ function Portal() {
 
   const scale = Math.min(1, 0.01 + scroll / 5000);
 
+  const handleMessageClick = (e) => {
+    setShowForm(true);
+    setShowMessage(false);
+    e.preventDefault();
+  };
+
+  const handleIdentifyYourself = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="Portal">
-      <a
-        href="/derive"
+     {showMessage && (
+      <div
         className="portal-text"
         style={{
           transform: `translate(-50%, -50%) scale(${scale})`,
         }}
+        onClick={(e) => handleMessageClick(e)}
       >
         Quod Est Forma Somniorum
-      </a>
+      </div>
+     )}
+
+      {showForm && (
+        <form className="user-reg-form">
+          <button 
+            type="submit"
+            onClick={(e) => handleIdentifyYourself(e)}
+          >
+            Identify yourself,
+          </button>
+          <input type="text" placeholder="Stranger" />
+        </form>
+      )}
 
       <div className="scroll-space"></div>
     </div>
