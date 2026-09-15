c0e8d764c8aa964e1d54454cebd64fdcd7844115
zocalop <jcb2861@gmail.com>
2026-09-14 23:06:01 -0300

    Create frontend user registration API in PortalAPI.js

--- src/Components/Portal/PortalAPI.js
+++ src/Components/Portal/PortalAPI.js
@@ -19,3 +19,29 @@ export const login = async (username, password) => {
   return response.text();
 
 };
+
+export const register = asyc (firstName, lastName, cart) => {
+  const params = new URLSearchParams({
+    firstName,
+    lastName
+  });
+
+  const response = await fetch(
+    `http://localhost:5000/register?${params}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        cart
+      })
+  });
+
+  if (!response.ok) {
+    throw new Error(await response.text());
+  }
+
+  return response.json();
+
+};
